b7f0060ce894074bd01ee6a0e5e78fa2f20ce811
Francisco1asir <fparnav1908@g.educaand.es>
2024-02-13 13:13:36 +0100

    push1

---
+++ nextclass-main/src/app/public/productos/page.tsx
@@ -0,0 +1,28 @@
+import { IProducto } from '@/interfaces/IProductos'
+import { getLibros } from '@/model/libros/datalibro'
+import React from 'react'
+
+async function Productospage() {
+  // async function getProductos() {
+  //   const res = await fetch('http://localhost:3001/api/libros/listar', { cache: 'no-store' })
+  //   if (!res.ok) {
+  //     throw new Error('Failed to fetch data')
+  //   }
+  //   return res.json()
+  // }
+  const productos: IProducto[] = await getLibros()
+
+  return (
+    <>
+      <h1 className='h1productos'>API PRODUCTOS</h1>
+      <ul>
+        {productos.map((producto: IProducto) => (
+          <li>
+            {producto.title}
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
+export default Productospage
